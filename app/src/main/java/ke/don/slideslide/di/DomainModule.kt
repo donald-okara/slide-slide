cfb035b17f6157c51f923c7269ceb3cc3715a05c
Jeremy <jeremyjey254@gmail.com>
2026-08-14 21:57:55 +0300

    feat:✨ change PuzzleManager binding scope to ViewModelScoped

--- app/src/main/java/ke/don/slideslide/di/DomainModule.kt
+++ app/src/main/java/ke/don/slideslide/di/DomainModule.kt
@@ -18,6 +18,7 @@ package ke.don.slideslide.di
 import dagger.Binds
 import dagger.Module
 import dagger.hilt.InstallIn
+import dagger.hilt.android.scopes.ViewModelScoped
 import dagger.hilt.components.SingletonComponent
 import ke.don.slideslide.domain.manager.PuzzleManager
 import ke.don.slideslide.domain.manager.PuzzleManagerImpl
@@ -51,6 +52,6 @@ abstract class DomainModule {
     abstract fun bindValidateMoveUseCase(impl: ValidateMoveUseCaseImpl): ValidateMoveUseCase
 
     @Binds
-    @Singleton
+    @ViewModelScoped
     abstract fun bindPuzzleManager(impl: PuzzleManagerImpl): PuzzleManager
 }
